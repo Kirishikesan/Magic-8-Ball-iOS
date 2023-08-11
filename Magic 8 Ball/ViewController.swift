a84284d8fbd01111f5629c59a7d288fcf01256cf
Kannan Kirishikesan <kannan@wso2.com>
2023-08-11 10:14:31 +0530

    Adds magic 8 ball application

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var magic8BallImage: UIImageView!
+    
+    let ballArray = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5")
+    ]
 
+    override func viewDidLoad() {
+        magic8BallImage.image = ballArray.randomElement()
+    }
 
-
+    @IBAction func askButtonClicked(_ sender: Any) {
+        magic8BallImage.image = ballArray.randomElement()
+    }
 }
 
